25d65de2b192312e004a16b1693142b78a2c32da
Naleen Dissanayake <naleenituom@gmail.com>
2026-01-15 19:26:34 +0900

    SwiftData Sample App Project initialization

---
+++ SwiftDataSampleApp/SwiftDataSampleApp/SwiftDataSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSampleAppApp.swift
+//  SwiftDataSampleApp
+//
+//  Created by Naleen Dissanayake on 2026/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
