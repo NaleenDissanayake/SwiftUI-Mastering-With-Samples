d8977fb89ecbe6bc5b952e5a59e0f5a28b48c0f2
Naleen Dissanayake <naleenituom@gmail.com>
2026-01-15 20:39:03 +0900

    Integrate data model into the view

--- SwiftDataSampleApp/SwiftDataSampleApp/ContentView.swift
+++ SwiftDataSampleApp/SwiftDataSampleApp/ContentView.swift
@@ -6,19 +6,47 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var wishList: [WishModel]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(wishList) { wish in
+                    Text(wish.title)
+                        .padding(10)
+                }
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Wish List")
+            .overlay {
+                if wishList.isEmpty {
+                    ContentUnavailableView {
+                        Label("No wishes to show.", systemImage: "heart.fill")
+                    } description: {
+                        Text("Add a new wish!")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
+#Preview("Wish List") {
+    let container = try! ModelContainer(for: WishModel.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+                                             
+    container.mainContext.insert(WishModel(title: "A very deep thought"))
+    container.mainContext.insert(WishModel(title: "A very deep thought 2"))
+    container.mainContext.insert(WishModel(title: "A very deep thought 3"))
+    
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Empty Wish List") {
     ContentView()
+        .modelContainer(for: WishModel.self, inMemory: true)
 }
