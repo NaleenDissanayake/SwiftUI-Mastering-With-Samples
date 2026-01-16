244c7fb5aacc62f07ae50d05c1453d68a28a83cb
Naleen Dissanayake <naleenituom@gmail.com>
2026-01-16 18:24:23 +0900

    Basic Project Structure for SriLankaNature App

---
+++ SriLankaNature/SriLankaNature/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SriLankaNature
+//
+//  Created by Naleen Dissanayake on 2026/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SriLankaNature/SriLankaNature/SriLankaNatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SriLankaNatureApp.swift
+//  SriLankaNature
+//
+//  Created by Naleen Dissanayake on 2026/01/16.
+//
+
+import SwiftUI
+
+@main
+struct SriLankaNatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
